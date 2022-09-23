f3296c7d38e9a89de553f3104a29e64a9ec68fbf
TALEB Elias <elias.taleb@thalesgroup.com>
2022-09-23 15:39:16 +0200

    add cname for wab

--- wab.tf
+++ wab.tf
@@ -19,3 +19,13 @@ resource "aws_route53_record" "wab" {
   ttl     = 300
   records = [aws_instance.wab.private_ip]
 }
+
+resource "aws_route53_record" "wabcname" {
+  zone_id = aws_route53_zone.private.zone_id
+  name    = "wab"
+  type    = "CNAME"
+  ttl     = 300
+  records = [
+    aws_route53_record.wab.name,
+  ]
+}
