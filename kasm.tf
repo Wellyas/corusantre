b64304c4f8b0b5773522e66cf6c45d30c8f815bf
TALEB Elias <elias.taleb@thalesgroup.com>
2022-09-23 16:57:09 +0200

    Change SSH acces

--- kasm.tf
+++ kasm.tf
@@ -3,7 +3,7 @@ module "kasmweb" {
   vpc_id              = aws_vpc.sidera_cloud.id
   internet_gateway_id = aws_internet_gateway.gw.id
   key_name            = aws_key_pair.ssh.key_name
-  ssh_access_cidr = "10.135.190.0/23"
+  ssh_access_cidr = aws_subnet.sc_access.cidr_block
   https_access_cidr = [ "176.161.232.174/32", "92.184.112.0/24", "176.149.95.90/32", "192.54.145.0/24","192.54.200.0/24"]
   vpn_fw_oam = aws_vpn_gateway.sideracloud_vpngw.id
   private_zone_id = aws_route53_zone.private.id
