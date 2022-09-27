fe0b3f677eb334a1bc4b54d0433ebc970428c3ba
TALEB Elias <elias.taleb@thalesgroup.com>
2022-09-27 13:09:21 +0200

    Add ecstask role

--- ecs.tf
+++ ecs.tf
@@ -62,6 +62,6 @@ resource "aws_iam_role" "ecsTaskExecutionRole" {
   name                = "ecsTaskExecutionRole"
   assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json
   managed_policy_arns = [
-      aws_iam_policy.ecs_task_execution_policy.arn
+      data.aws_iam_policy.ecs_task_execution_policy.arn
   ]
 }
