fa57d7bbbf88705ab912783ade23a54e02104022
TALEB Elias <elias.taleb@thalesgroup.com>
2022-09-29 15:17:33 +0200

    add loadbalancer ecs

--- ecs-lb.tf
+++ ecs-lb.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "ecs_lb" {
-  name               = "ecs_lb"
+  name               = "ecs-lb"
   internal           = true
   load_balancer_type = "network"
   subnets            = aws_subnet.sc_ecs_lb.*.id
