b7bebfcea8b1a8babdd8fc11111de5c7e615268b
TALEB Elias <elias.taleb@thalesgroup.com>
2022-09-29 19:10:02 +0200

    add network acl 5

--- ldap.tf
+++ ldap.tf
@@ -147,6 +147,7 @@ resource "aws_route_table_association" "nat" {
 
 resource "aws_lb_target_group" "ldap" {
   name_prefix     = "ldap"
+  port     = 1389
   target_type = "ip"
   protocol = "TCP"
   vpc_id   = aws_vpc.sidera_cloud.id
