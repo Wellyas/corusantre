e3d7908327482c79e33b2e8ba1f5b12504d278ea
TALEB Elias <elias.taleb@thalesgroup.com>
2022-09-30 14:48:38 +0200

    test add healtceck 4

--- ldap.tf
+++ ldap.tf
@@ -38,7 +38,7 @@ resource "aws_ecs_task_definition" "ldap" {
     ],
     "healthCheck" : {
       "command" : [ "CMD-SHELL", "opendj/bin/ldapsearch --hostname localhost --port 1636 --bindDN '$ROOT_USER_DN' --bindPassword '$ROOT_PASSWORD' --useSsl --trustAll --baseDN '$BASE_DN' --searchScope base '(objectClass=*)' 1.1 || exit 1" ],
-      "timeout": 30,
+      "timeout": 30
     },
     "logConfiguration": {
       "logDriver": "awslogs",
