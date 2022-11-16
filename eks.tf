a5bffceab124f1d622e6d2f9d23195e9791511d1
TALEB Elias <elias.taleb@thalesgroup.com>
2022-11-16 18:49:30 +0100

    First try EKS

---
+++ eks.tf
@@ -0,0 +1,80 @@
+resource "aws_subnet" "sc_eks" {
+  count = 2
+  private_dns_hostname_type_on_launch = "resource-name"
+  availability_zone = data.aws_availability_zones.zone.names[count.index]
+  vpc_id     = aws_vpc.sidera_cloud.id
+  cidr_block        = cidrsubnet(aws_vpc.sidera_cloud.cidr_block, 12, count.index+20)
+
+  tags = {
+    Name  = "Zone EKS"
+  }
+}
+
+locals {
+  cluster_name = "sidera-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.26.6"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.22"
+  
+  cluster_endpoint_private_access = true 
+  
+  vpc_id     = aws_vpc.sidera_cloud.id
+  subnet_ids = aws_subnet.sc_eks.*.id
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+    attach_cluster_primary_security_group = true
+
+    # Disabling and using externally provided security groups
+    create_security_group = false
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      pre_bootstrap_user_data = <<-EOT
+      echo 'foo bar'
+      EOT
+
+      vpc_security_group_ids = [
+        aws_security_group.sg_admin_from_wab.id
+      ]
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      pre_bootstrap_user_data = <<-EOT
+      echo 'foo bar'
+      EOT
+
+      vpc_security_group_ids = [
+        aws_security_group.sg_admin_from_wab.id
+      ]
+    }
+  }
+}
