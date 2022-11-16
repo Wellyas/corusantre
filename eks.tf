8b0dcf9a5b68c514b516a8543121387bd2df1540
TALEB Elias <elias.taleb@thalesgroup.com>
2022-11-16 22:23:06 +0100

    second try EKS

--- eks.tf
+++ eks.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "sc_eks" {
+/* resource "aws_subnet" "sc_eks" {
   count = 2
   private_dns_hostname_type_on_launch = "resource-name"
   availability_zone = data.aws_availability_zones.zone.names[count.index]
@@ -77,4 +77,4 @@ module "eks" {
       ]
     }
   }
-}
+} */
